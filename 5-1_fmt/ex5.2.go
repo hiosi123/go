26462022bf087ae7a17d376a089310540ce00311
hiosi123 <tls1641@gmail.com>
2022-09-06 23:44:53 +0900

    go

---
+++ 5-1_fmt/ex5.2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int = 20
+	var f float64 = 321.123
+	fmt.Print("a:", a, "b:", b, "c:", f)
+	fmt.Printf("a: %d b: %d f:%f \n", a, b, f)
+
+}
